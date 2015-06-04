0ce83574d9615d606fbebac59ae31cf92dc3e6e3
patcat <patrick@patcatcreative.com>
2015-06-04 15:31:49 +1000

    Random pixel background working

--- src/main.c
+++ src/main.c
@@ -1,84 +1,214 @@
 #include <pebble.h>
- 
-static Window *window;
-static TextLayer *time_layer;
- 
-// http://stackoverflow.com/questions/21150193/logging-enums-on-the-pebble-watch/21172222#21172222
-char *translate_error(AppMessageResult result) {
-  switch (result) {
-    case APP_MSG_OK: return "APP_MSG_OK";
-    case APP_MSG_SEND_TIMEOUT: return "APP_MSG_SEND_TIMEOUT";
-    case APP_MSG_SEND_REJECTED: return "APP_MSG_SEND_REJECTED";
-    case APP_MSG_NOT_CONNECTED: return "APP_MSG_NOT_CONNECTED";
-    case APP_MSG_APP_NOT_RUNNING: return "APP_MSG_APP_NOT_RUNNING";
-    case APP_MSG_INVALID_ARGS: return "APP_MSG_INVALID_ARGS";
-    case APP_MSG_BUSY: return "APP_MSG_BUSY";
-    case APP_MSG_BUFFER_OVERFLOW: return "APP_MSG_BUFFER_OVERFLOW";
-    case APP_MSG_ALREADY_RELEASED: return "APP_MSG_ALREADY_RELEASED";
-    case APP_MSG_CALLBACK_ALREADY_REGISTERED: return "APP_MSG_CALLBACK_ALREADY_REGISTERED";
-    case APP_MSG_CALLBACK_NOT_REGISTERED: return "APP_MSG_CALLBACK_NOT_REGISTERED";
-    case APP_MSG_OUT_OF_MEMORY: return "APP_MSG_OUT_OF_MEMORY";
-    case APP_MSG_CLOSED: return "APP_MSG_CLOSED";
-    case APP_MSG_INTERNAL_ERROR: return "APP_MSG_INTERNAL_ERROR";
-    default: return "UNKNOWN ERROR";
-  }
-}
- 
-static void init_ui(Window *window) {
-  
-  // time_layer
-  time_layer = text_layer_create(GRect(0, 29, 144, 54));
-  text_layer_set_background_color(time_layer, GColorClear);
-  text_layer_set_text_color(time_layer, GColorWhite);
-  text_layer_set_text(time_layer, "");
-  text_layer_set_text_alignment(time_layer, GTextAlignmentCenter);
-  layer_add_child(window_get_root_layer(window), (Layer *)time_layer);
+
+#define COLORS       true
+#define ANTIALIASING true
+
+#define HAND_MARGIN  10
+#define FINAL_RADIUS 55
+
+#define ANIMATION_DURATION 500
+#define ANIMATION_DELAY    600
+
+#define PIXEL_COUNT 12
+
+typedef struct {
+  int hours;
+  int minutes;
+} Time;
+
+static Window *s_main_window;
+static Layer *s_canvas_layer;
+
+static GPoint s_center;
+static Time s_last_time, s_anim_time;
+static int s_radius = 0, s_anim_hours_60 = 0, random_colors[20][20][3];
+static bool s_animating = false;
+
+/*************************** AnimationImplementation **************************/
+
+static void animation_started(Animation *anim, void *context) {
+  s_animating = true;
 }
 
-static void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
-  static char time_text[] = "00:00";
- 
-  strftime(time_text, sizeof(time_text), "%I:%M", tick_time);
-  text_layer_set_text(time_layer, time_text);
+static void animation_stopped(Animation *anim, bool stopped, void *context) {
+  s_animating = false;
 }
- 
-static void init_clock(Window *window) {
-  time_t now = time(NULL);
-  struct tm *current_time = localtime(&now);
-  tick_timer_service_subscribe(MINUTE_UNIT, &handle_minute_tick);
+
+static void animate(int duration, int delay, AnimationImplementation *implementation, bool handlers) {
+  Animation *anim = animation_create();
+  animation_set_duration(anim, duration);
+  animation_set_delay(anim, delay);
+  animation_set_curve(anim, AnimationCurveEaseInOut);
+  animation_set_implementation(anim, implementation);
+  if(handlers) {
+    animation_set_handlers(anim, (AnimationHandlers) {
+      .started = animation_started,
+      .stopped = animation_stopped
+    }, NULL);
+  }
+  animation_schedule(anim);
 }
- 
+
+/************************************ UI **************************************/
+
+static void tick_handler(struct tm *tick_time, TimeUnits changed) {
+  // Store time
+  s_last_time.hours = tick_time->tm_hour;
+  s_last_time.hours -= (s_last_time.hours > 12) ? 12 : 0;
+  s_last_time.minutes = tick_time->tm_min;
+
+  /*for(int i = 0; i < 3; i++) {
+    s_color_channels[i] = rand() % 256;
+  }*/
+
+  // Redraw
+  if(s_canvas_layer) {
+    layer_mark_dirty(s_canvas_layer);
+  }
+}
+
+static int hours_to_minutes(int hours_out_of_12) {
+  return (int)(float)(((float)hours_out_of_12 / 12.0F) * 60.0F);
+}
+
+static void update_proc(Layer *layer, GContext *ctx) {
+  int i, j;
+  GRect bounds = layer_get_bounds(layer);
+  int cube_size = bounds.size.w / PIXEL_COUNT;
+
+  for (i = 0; i < 20; i++) {
+    for (j = 0; j < 20; j++) {
+      random_colors[i][j][0] = rand() % 256;
+      random_colors[i][j][1] = 0;
+      random_colors[i][j][2] = 0;
+
+      graphics_context_set_fill_color(ctx, GColorFromRGB(random_colors[i][j][0], random_colors[i][j][1], random_colors[i][j][2]));
+      //graphics_context_set_fill_color(ctx, GColorFromRGB(0,0,255));
+      int x = i*cube_size;
+      int y = j*cube_size;
+      graphics_fill_rect(ctx, GRect(x, y, (x+cube_size), (y+cube_size)), 0, GCornerNone);
+    }
+  }
+
+  // Color background?
+  /*if(COLORS) {
+    graphics_context_set_fill_color(ctx, GColorFromRGB(s_color_channels[0], s_color_channels[1], s_color_channels[2]));
+    graphics_fill_rect(ctx, GRect(0, 0, 144, 168), 0, GCornerNone);
+  }*/
+
+  graphics_context_set_stroke_color(ctx, GColorBlack);
+  graphics_context_set_stroke_width(ctx, 4);
+
+  graphics_context_set_antialiased(ctx, ANTIALIASING);
+
+  // White clockface
+  graphics_context_set_fill_color(ctx, GColorWhite);
+  graphics_fill_circle(ctx, s_center, s_radius);
+
+  // Draw outline
+  graphics_draw_circle(ctx, s_center, s_radius);
+
+  // Don't use current time while animating
+  Time mode_time = (s_animating) ? s_anim_time : s_last_time;
+
+  // Adjust for minutes through the hour
+  float minute_angle = TRIG_MAX_ANGLE * mode_time.minutes / 60;
+  float hour_angle;
+  if(s_animating) {
+    // Hours out of 60 for smoothness
+    hour_angle = TRIG_MAX_ANGLE * mode_time.hours / 60;
+  } else {
+    hour_angle = TRIG_MAX_ANGLE * mode_time.hours / 12;
+  }
+  hour_angle += (minute_angle / TRIG_MAX_ANGLE) * (TRIG_MAX_ANGLE / 12);
+
+  // Plot hands
+  GPoint minute_hand = (GPoint) {
+    .x = (int16_t)(sin_lookup(TRIG_MAX_ANGLE * mode_time.minutes / 60) * (int32_t)(s_radius - HAND_MARGIN) / TRIG_MAX_RATIO) + s_center.x,
+    .y = (int16_t)(-cos_lookup(TRIG_MAX_ANGLE * mode_time.minutes / 60) * (int32_t)(s_radius - HAND_MARGIN) / TRIG_MAX_RATIO) + s_center.y,
+  };
+  GPoint hour_hand = (GPoint) {
+    .x = (int16_t)(sin_lookup(hour_angle) * (int32_t)(s_radius - (2 * HAND_MARGIN)) / TRIG_MAX_RATIO) + s_center.x,
+    .y = (int16_t)(-cos_lookup(hour_angle) * (int32_t)(s_radius - (2 * HAND_MARGIN)) / TRIG_MAX_RATIO) + s_center.y,
+  };
+
+  // Draw hands with positive length only
+  if(s_radius > 2 * HAND_MARGIN) {
+    graphics_draw_line(ctx, s_center, hour_hand);
+  } 
+  if(s_radius > HAND_MARGIN) {
+    graphics_draw_line(ctx, s_center, minute_hand);
+  }
+}
+
 static void window_load(Window *window) {
-  init_ui(window);
-  init_clock(window);
+  Layer *window_layer = window_get_root_layer(window);
+  GRect window_bounds = layer_get_bounds(window_layer);
+
+  s_center = grect_center_point(&window_bounds);
+
+  s_canvas_layer = layer_create(window_bounds);
+  layer_set_update_proc(s_canvas_layer, update_proc);
+  layer_add_child(window_layer, s_canvas_layer);
 }
- 
+
 static void window_unload(Window *window) {
-  window_destroy(window);
-  text_layer_destroy(time_layer);
+  layer_destroy(s_canvas_layer);
 }
- 
-static void init(void) {
-  window = window_create();
-  window_set_window_handlers(window, (WindowHandlers) {
+
+/*********************************** App **************************************/
+
+static int anim_percentage(AnimationProgress dist_normalized, int max) {
+  return (int)(float)(((float)dist_normalized / (float)ANIMATION_NORMALIZED_MAX) * (float)max);
+}
+
+static void radius_update(Animation *anim, AnimationProgress dist_normalized) {
+  s_radius = anim_percentage(dist_normalized, FINAL_RADIUS);
+
+  layer_mark_dirty(s_canvas_layer);
+}
+
+static void hands_update(Animation *anim, AnimationProgress dist_normalized) {
+  s_anim_time.hours = anim_percentage(dist_normalized, hours_to_minutes(s_last_time.hours));
+  s_anim_time.minutes = anim_percentage(dist_normalized, s_last_time.minutes);
+
+  layer_mark_dirty(s_canvas_layer);
+}
+
+static void init() {
+  srand(time(NULL));
+
+  time_t t = time(NULL);
+  struct tm *time_now = localtime(&t);
+  tick_handler(time_now, MINUTE_UNIT);
+
+  s_main_window = window_create();
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
     .load = window_load,
     .unload = window_unload,
   });
- 
-  const bool animated = true;
-  window_stack_push(window, animated);
+  window_stack_push(s_main_window, true);
 
-  #ifdef PBL_COLOR
-    window_set_background_color(window, GColorIndigo);
-  #else
-    window_set_background_color(window, GColorBlack);
-  #endif
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+
+  // Prepare animations
+  AnimationImplementation radius_impl = {
+    .update = radius_update
+  };
+  animate(ANIMATION_DURATION, ANIMATION_DELAY, &radius_impl, false);
+
+  AnimationImplementation hands_impl = {
+    .update = hands_update
+  };
+  animate(2 * ANIMATION_DURATION, ANIMATION_DELAY, &hands_impl, true);
 }
- 
-int main(void) {
+
+static void deinit() {
+  window_destroy(s_main_window);
+}
+
+int main() {
   init();
- 
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
- 
   app_event_loop();
+  deinit();
 }
